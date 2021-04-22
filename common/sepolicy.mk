e2d6f55edaf4186970923811b981aee505693640
Gagan Malvi <malvi@aospa.co>
2021-04-22 05:05:22 +0000

    sepolicy: Update paths for Styx
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -16,20 +16,20 @@ endif
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/potato/sepolicy/common/public
+    device/styx/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/potato/sepolicy/common/private
+    device/styx/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/potato/sepolicy/common/dynamic \
-    device/potato/sepolicy/common/system
+    device/styx/sepolicy/common/dynamic \
+    device/styx/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/potato/sepolicy/common/dynamic \
-    device/potato/sepolicy/common/vendor
+    device/styx/sepolicy/common/dynamic \
+    device/styx/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/potato/sepolicy/legacy-common/sepolicy.mk
+-include device/styx/sepolicy/legacy-common/sepolicy.mk
